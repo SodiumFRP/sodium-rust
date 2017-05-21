4a100c6968d73a07a056f23528681943affc5030
Clinton Selke <clinuxrulz@gmail.com>
2017-05-21 15:42:45 +1000

    start of typescript test translation

--- src/rusty_frp/mod.rs
+++ src/rusty_frp/mod.rs
@@ -2,6 +2,7 @@ pub use self::frp::Cell;
 pub use self::frp::CellSink;
 pub use self::frp::CellTrait;
 pub use self::frp::FrpContext;
+pub use self::frp::StreamSink;
 pub use self::frp::StreamTrait;
 pub use self::frp::WithFrpContext;
 
