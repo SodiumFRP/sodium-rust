969cff5c3cf4026f14b4cc2811dc433dc5a0146e
Teymour Aldridge <teymour.aldridge@icloud.com>
2020-06-06 11:04:52 +0100

    Allow mutable keys.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+#![allow(clippy::mutable_key_type)]
 #[macro_use]
 extern crate log;
 
-mod impl_;
 mod cell;
 mod cell_loop;
 mod cell_sink;
+mod impl_;
 mod listener;
 mod operational;
 mod sodium_ctx;
@@ -16,6 +17,12 @@ pub use self::cell::Cell;
 pub use self::cell_loop::CellLoop;
 pub use self::cell_sink::CellSink;
 pub use self::impl_::dep::Dep;
+pub use self::impl_::lambda::lambda1;
+pub use self::impl_::lambda::lambda2;
+pub use self::impl_::lambda::lambda3;
+pub use self::impl_::lambda::lambda4;
+pub use self::impl_::lambda::lambda5;
+pub use self::impl_::lambda::lambda6;
 pub use self::impl_::lambda::IsLambda1;
 pub use self::impl_::lambda::IsLambda2;
 pub use self::impl_::lambda::IsLambda3;
@@ -23,12 +30,6 @@ pub use self::impl_::lambda::IsLambda4;
 pub use self::impl_::lambda::IsLambda5;
 pub use self::impl_::lambda::IsLambda6;
 pub use self::impl_::lambda::Lambda;
-pub use self::impl_::lambda::lambda1;
-pub use self::impl_::lambda::lambda2;
-pub use self::impl_::lambda::lambda3;
-pub use self::impl_::lambda::lambda4;
-pub use self::impl_::lambda::lambda5;
-pub use self::impl_::lambda::lambda6;
 pub use self::impl_::lazy::Lazy;
 pub use self::impl_::node::Node;
 pub use self::listener::Listener;
