423e57d0a287ff02f807607c3274cb71c83a7a6b
Zephyr Shannon <earthlingzephyr@gmail.com>
2020-12-27 18:37:40 -0800

    Document all CellSink methods

--- src/cell_sink.rs
+++ src/cell_sink.rs
@@ -3,7 +3,7 @@ use crate::impl_::cell_sink::CellSink as CellSinkImpl;
 use crate::sodium_ctx::SodiumCtx;
 
 /// A [`Cell`] that allows values to be pushed into it, acting as a
-/// bridge between the world of I/O and the world of FRP.
+/// interface between the world of I/O and the world of FRP.
 ///
 /// ## Note: This should only be used from _outside_ the context of
 /// the Sodium system to inject data from I/O into the reactive system.
@@ -20,18 +20,30 @@ impl<A> Clone for CellSink<A> {
 }
 
 impl<A: Clone + Send + 'static> CellSink<A> {
+    /// Create a new `CellSink` in the given context.
     pub fn new(sodium_ctx: &SodiumCtx, a: A) -> CellSink<A> {
         CellSink {
             impl_: CellSinkImpl::new(&sodium_ctx.impl_, a),
         }
     }
 
+    /// Return a [`Cell`] that can be used to create Sodium logic that
+    /// will read the values pushed into this `CellSink` from the I/O
+    /// world.
     pub fn cell(&self) -> Cell<A> {
         Cell {
             impl_: self.impl_.cell(),
         }
     }
 
+    /// Send a value, modifying the value of the cell.
+    ///
+    /// This method may not be called in handlers registered with
+    /// [`Stream::listen`][crate::Stream::listen] or [`Cell::listen`].
+    ///
+    /// `CellSink` is an operational primitive, meant for interfacing
+    /// I/O to FRP only. You aren't meant to use this to define your
+    /// own primitives.
     pub fn send(&self, a: A) {
         self.impl_.send(a);
     }
