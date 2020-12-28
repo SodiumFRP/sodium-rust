f7d556360d2c2fcda81116327a0a8ecdebfcea5c
Zephyr Shannon <earthlingzephyr@gmail.com>
2020-12-27 20:09:41 -0800

    Document all StreamLoop methods

--- src/stream_loop.rs
+++ src/stream_loop.rs
@@ -8,18 +8,30 @@ pub struct StreamLoop<A> {
 }
 
 impl<A: Send + Clone + 'static> StreamLoop<A> {
+    /// Create a new `StreamLoop` in the given context.
     pub fn new(sodium_ctx: &SodiumCtx) -> StreamLoop<A> {
         StreamLoop {
             impl_: StreamLoopImpl::new(&sodium_ctx.impl_),
         }
     }
 
+    /// Return a [`Stream`] that is equivalent to this `StreamLoop`
+    /// once it has been resolved by calling
+    /// [`loop_`][StreamLoop::loop_].
     pub fn stream(&self) -> Stream<A> {
         Stream {
             impl_: self.impl_.stream(),
         }
     }
 
+    /// Resolve the loop to specify what this `StreamLoop` was a
+    /// forward reference to.
+    ///
+    /// This function _must_ be invoked in the same transaction as the
+    /// place where the `StreamLoop` is used. This requires you to
+    /// create an explicit transaction, either with
+    /// [`SodiumCtx::transaction`] or
+    /// [`Transaction::new`][crate::Transaction::new].
     pub fn loop_(&self, sa: &Stream<A>) {
         self.impl_.loop_(&sa.impl_);
     }
