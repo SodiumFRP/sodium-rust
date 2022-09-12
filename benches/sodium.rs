82f667530c044dea21ff562a94547649781e4bd9
Zefira Shannon <earthlingzephyr@gmail.com>
2022-09-12 14:21:23 -0700

    Add benchmarks for Streams with filter and map & filter

--- benches/sodium.rs
+++ benches/sodium.rs
@@ -38,6 +38,43 @@ fn stream(c: &mut Criterion) {
             (ctx, sink, listener)
         })
     });
+
+    stream_send.bench_function("+filter", |b| {
+        b.iter_with_large_drop(|| {
+            let ctx = SodiumCtx::new();
+            let sink = ctx.new_stream_sink();
+
+            let mut values: Vec<u16> = Vec::new();
+            let listener = sink
+                .stream()
+                .filter(|v: &u16| *v % 3 == 0 && *v % 5 == 0)
+                .listen(move |v: &u16| values.push(black_box(*v)));
+
+            for v in 0_u16..1000 {
+                sink.send(black_box(v));
+            }
+            (ctx, sink, listener)
+        })
+    });
+
+    stream_send.bench_function("+map+filter", |b| {
+        b.iter_with_large_drop(|| {
+            let ctx = SodiumCtx::new();
+            let sink = ctx.new_stream_sink();
+
+            let mut values: Vec<u16> = Vec::new();
+            let listener = sink
+                .stream()
+                .map(|v: &u16| *v + 1)
+                .filter(|v: &u16| *v % 3 == 0 && *v % 5 == 0)
+                .listen(move |v: &u16| values.push(black_box(*v)));
+
+            for v in 0_u16..1000 {
+                sink.send(black_box(v));
+            }
+            (ctx, sink, listener)
+        })
+    });
 }
 
 fn cell(c: &mut Criterion) {
