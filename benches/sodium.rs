2e5b83d8e9c77d594193893b351fb185a4fcfc4b
Zefira Shannon <earthlingzephyr@gmail.com>
2022-09-12 14:17:03 -0700

    Compare simple send to send+map

--- benches/sodium.rs
+++ benches/sodium.rs
@@ -1,35 +1,76 @@
-use criterion::{criterion_group, criterion_main, black_box, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use sodium_rust::SodiumCtx;
 
 fn stream(c: &mut Criterion) {
-    c.bench_function("stream.send", |b| b.iter_with_large_drop(|| {
-        let ctx = SodiumCtx::new();
-        let sink = ctx.new_stream_sink();
+    let mut stream_send = c.benchmark_group("Stream::send");
+    stream_send.bench_function("simple", |b| {
+        b.iter_with_large_drop(|| {
+            let ctx = SodiumCtx::new();
+            let sink = ctx.new_stream_sink();
 
-        let mut values: Vec<u16> = Vec::new();
-        let listener = sink.stream().listen(move |v: &u16| values.push(black_box(*v)));
+            let mut values: Vec<u16> = Vec::new();
+            let listener = sink
+                .stream()
+                .listen(move |v: &u16| values.push(black_box(*v)));
 
-        for v in 0_u16..1000 {
-            sink.send(black_box(v));
-        }
-        (ctx, sink, listener)
-    }));
+            for v in 0_u16..1000 {
+                sink.send(black_box(v));
+            }
+            (ctx, sink, listener)
+        })
+    });
+
+    stream_send.bench_function("+map", |b| {
+        b.iter_with_large_drop(|| {
+            let ctx = SodiumCtx::new();
+            let sink = ctx.new_stream_sink();
+
+            let mut values: Vec<u16> = Vec::new();
+            let listener = sink
+                .stream()
+                .map(|v: &u16| *v + 10)
+                .listen(move |v: &u16| values.push(black_box(*v)));
+
+            for v in 0_u16..1000 {
+                sink.send(black_box(v));
+            }
+            (ctx, sink, listener)
+        })
+    });
 }
 
 fn cell(c: &mut Criterion) {
-    c.bench_function("cell.send", |b| b.iter_with_large_drop(|| {
-        let ctx = SodiumCtx::new();
+    let mut cell_send = c.benchmark_group("Cell::send");
+    cell_send.bench_function("simple", |b| {
+        b.iter_with_large_drop(|| {
+            let ctx = SodiumCtx::new();
 
-        let sink = ctx.new_cell_sink(0_u16);
-        let cell = sink.cell();
+            let sink = ctx.new_cell_sink(0_u16);
+            let cell = sink.cell();
 
-        for v in 0_u16..1000 {
-            sink.send(black_box(v));
-            let s = black_box(cell.sample());
-            assert_eq!(v, s);
-        }
-    }));
+            for v in 0_u16..1000 {
+                sink.send(black_box(v));
+                let s = black_box(cell.sample());
+                assert_eq!(v, s);
+            }
+        })
+    });
+
+    cell_send.bench_function("+map", |b| {
+        b.iter_with_large_drop(|| {
+            let ctx = SodiumCtx::new();
+
+            let sink = ctx.new_cell_sink(0_u16);
+            let cell = sink.cell().map(|v: &u16| *v + 10);
+
+            for v in 0_u16..1000 {
+                sink.send(black_box(v));
+                let s = black_box(cell.sample());
+                assert_eq!(v + 10, s);
+            }
+        })
+    });
 }
 
 criterion_group!(benches, stream, cell);
