fb1569b3b7332e0b1816de2d5cdf606b07949361
Zefira Shannon <earthlingzephyr@gmail.com>
2022-09-15 01:46:09 -0700

    Add benchmarks for all snapshot fns

--- benches/sodium.rs
+++ benches/sodium.rs
@@ -226,5 +226,157 @@ fn cell(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, cell, stream);
+fn snapshot(c: &mut Criterion) {
+    let mut snapshot = c.benchmark_group("snapshot");
+    snapshot.bench_function("one", |b| {
+        b.iter(|| {
+            let ctx = SodiumCtx::new();
+
+            let stream_sink = ctx.new_stream_sink();
+            let cell_sink = ctx.new_cell_sink(0_u16);
+            let sum_stream = stream_sink
+                .stream()
+                .snapshot(&cell_sink.cell(), |v: &u16, add: &u16| *v + *add);
+
+            let mut values: Vec<u16> = Vec::new();
+            let _listener = sum_stream.listen(move |v: &u16| values.push(black_box(*v)));
+
+            for add in 1_u16..50 {
+                for v in 0_u16..100 {
+                    stream_sink.send(v);
+                }
+                cell_sink.send(add);
+            }
+        });
+    });
+    snapshot.bench_function("two", |b| {
+        b.iter(|| {
+            let ctx = SodiumCtx::new();
+
+            let stream_sink = ctx.new_stream_sink();
+            let ca = ctx.new_cell_sink(0_u16);
+            let cb = ctx.new_cell_sink(10_u16);
+            let sum_stream = stream_sink.stream().snapshot3(
+                &ca.cell(),
+                &cb.cell(),
+                |v: &u16, a: &u16, b: &u16| *v + *a + *b,
+            );
+
+            let mut values: Vec<u16> = Vec::new();
+            let _listener = sum_stream.listen(move |v: &u16| values.push(black_box(*v)));
+
+            for add in 1_u16..50 {
+                for v in 0_u16..100 {
+                    stream_sink.send(v);
+                }
+                if add % 2 == 0 {
+                    ca.send(add);
+                } else {
+                    cb.send(add + 10);
+                }
+            }
+        });
+    });
+    snapshot.bench_function("three", |b| {
+        b.iter(|| {
+            let ctx = SodiumCtx::new();
+
+            let stream_sink = ctx.new_stream_sink();
+            let ca = ctx.new_cell_sink(0_u16);
+            let cb = ctx.new_cell_sink(10_u16);
+            let cc = ctx.new_cell_sink(100_u16);
+            let sum_stream = stream_sink.stream().snapshot4(
+                &ca.cell(),
+                &cb.cell(),
+                &cc.cell(),
+                |v: &u16, a: &u16, b: &u16, c: &u16| *v + *a + *b + *c,
+            );
+
+            let mut values: Vec<u16> = Vec::new();
+            let _listener = sum_stream.listen(move |v: &u16| values.push(black_box(*v)));
+
+            for add in 1_u16..50 {
+                for v in 0_u16..100 {
+                    stream_sink.send(v);
+                }
+                match add % 3 {
+                    2 => ca.send(add),
+                    1 => cb.send(add + 10),
+                    _ => cc.send(add + 100),
+                }
+            }
+        });
+    });
+    snapshot.bench_function("four", |b| {
+        b.iter(|| {
+            let ctx = SodiumCtx::new();
+
+            let stream_sink = ctx.new_stream_sink();
+            let ca = ctx.new_cell_sink(0_u16);
+            let cb = ctx.new_cell_sink(10_u16);
+            let cc = ctx.new_cell_sink(100_u16);
+            let cd = ctx.new_cell_sink(1000_u16);
+            let sum_stream = stream_sink.stream().snapshot5(
+                &ca.cell(),
+                &cb.cell(),
+                &cc.cell(),
+                &cd.cell(),
+                |v: &u16, a: &u16, b: &u16, c: &u16, d: &u16| *v + *a + *b + *c + *d,
+            );
+
+            let mut values: Vec<u16> = Vec::new();
+            let _listener = sum_stream.listen(move |v: &u16| values.push(black_box(*v)));
+
+            for add in 1_u16..50 {
+                for v in 0_u16..100 {
+                    stream_sink.send(v);
+                }
+                match add % 4 {
+                    3 => ca.send(add),
+                    2 => cb.send(add + 10),
+                    1 => cc.send(add + 100),
+                    _ => cd.send(add + 1000),
+                }
+            }
+        });
+    });
+    snapshot.bench_function("five", |b| {
+        b.iter(|| {
+            let ctx = SodiumCtx::new();
+
+            let stream_sink = ctx.new_stream_sink();
+            let ca = ctx.new_cell_sink(0_u16);
+            let cb = ctx.new_cell_sink(10_u16);
+            let cc = ctx.new_cell_sink(100_u16);
+            let cd = ctx.new_cell_sink(1000_u16);
+            let ce = ctx.new_cell_sink(5_u16);
+            let sum_stream = stream_sink.stream().snapshot6(
+                &ca.cell(),
+                &cb.cell(),
+                &cc.cell(),
+                &cd.cell(),
+                &ce.cell(),
+                |v: &u16, a: &u16, b: &u16, c: &u16, d: &u16, e: &u16| *v + *a + *b + *c + *d + *e,
+            );
+
+            let mut values: Vec<u16> = Vec::new();
+            let _listener = sum_stream.listen(move |v: &u16| values.push(black_box(*v)));
+
+            for add in 1_u16..50 {
+                for v in 0_u16..100 {
+                    stream_sink.send(v);
+                }
+                match add % 5 {
+                    4 => ca.send(add),
+                    3 => cb.send(add + 10),
+                    2 => cc.send(add + 100),
+                    1 => cd.send(add + 1000),
+                    _ => ce.send(add + 5),
+                }
+            }
+        });
+    });
+}
+
+criterion_group!(benches, snapshot, cell, stream);
 criterion_main!(benches);
